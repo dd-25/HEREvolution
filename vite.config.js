5b11b0cd15c5acfb3d8e4c380854cf1c9969fc44
Dhruv Mundada <129663105+dd-25@users.noreply.github.com>
2024-04-06 12:30:40 +0530

    deployment try

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Configuration options here
+  base: '/HEREvolution/',
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist',
+    publicDir: 'public',
+    rollupOptions: {
+      input: 'src/main.js',
+    },
+    minify: 'terser',
+    manifest: true,
+  },
 });
